ca0475f8f20d7c2ef97b18ef7a9d8bbc93fe3e79
Davronbek Sharipov <davronking001@gmail.com>
2023-03-01 11:50:29 +0500

    Completed task1 List

--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -1,15 +1,28 @@
 package com.epam.mjc.collections.list;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        ListComparator comparator = new ListComparator();
+        sourceList.sort(comparator);
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int aInt = Integer.parseInt(a);
+        int bInt = Integer.parseInt(b);
+
+        int x = 5*aInt*aInt + 3;
+        int y = 5*bInt*bInt + 3;
+        if (x == y){
+            if (aInt > bInt){
+                return 1;
+            }else return -1;
+        } else if (x > y) {
+            return 1;
+        }else return -1;
     }
 }
